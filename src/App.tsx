59609b217287307bcce6cea76158d6c4c998358d
PlashSpeed-Aiman <maiman.ar@live.iium.edu.my>
2025-07-10 23:07:24 +0800

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import Header from './components/Header'
+import { Button } from './components/ui/button'
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className="text-center">
+        <Header/>
+      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+        <img
+          src={logo}
+          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
+          alt="logo"
+        />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="text-[#61dafb] hover:underline"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="text-[#61dafb] hover:underline"
+          href="https://tanstack.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant={"destructive"}>
+            Learn TanStack Router
+          </Button>
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
